7e42bb9168d3892da5f5cd2c8508324718088246
Yadi Anyankah <yanyankah@gmail.com>
2022-09-12 12:30:55 +0100

    Added my project

---
+++ src/main/java/com/yadi/springsecuritytutorial/security/PasswordConfig.java
@@ -0,0 +1,15 @@
+package com.yadi.springsecuritytutorial.security;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class PasswordConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
+}
